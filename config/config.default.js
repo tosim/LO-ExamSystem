6b51780bce44bb1805a9dcc82c9ea9ed097bbd4b
joey <82415327@qq.com>
2017-07-02 12:57:30 +0800

    init

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1498971103875_1650';
+
+  // add your config here
+
+  return config;
+};
