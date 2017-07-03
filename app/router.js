3c80a1790212863e36fd82f22e5cc7f1f653de7b
joey <82415327@qq.com>
2017-07-03 17:16:00 +0800

    remove all

--- app/router.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = app => {
-  app.get('/users/create',app.controller.test.createUser);
-  app.get('/', 'home.index');
-  app.resources('user','/api/users',app.controller.users);
-};
