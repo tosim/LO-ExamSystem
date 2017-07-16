54cfd429f4387e038ab5ed7554f15632e108b046
Yurisa <779509411@qq.com>
2017-07-16 11:45:37 +0800

    version1.1

--- app/web/html/professionlist/professionlist.vue
+++ app/web/html/professionlist/professionlist.vue
@@ -59,22 +59,24 @@ export default {
     },
     methods: {
        fetch(){
-            this.$http.get(`http://127.0.0.1:7001/getprofessionlist?page=${this.currpage}`).then(res => {
+            let _this = this;
+            _this.$http.get(`http://127.0.0.1:7001/getprofessionlist?page=${_this.currpage}`).then(res => {
+               
                    console.log(res)
-                    this.totalnum = res.data.data.totalnum;
-                    this.professionlist = res.data.data.professionlist;
+                    _this.totalnum = res.data.data.totalnum;
+                    _this.professionlist = res.data.data.professionlist;
                     console.log(res.data.data.professionlist);
-                    console.log(this.professionlist)
+                    console.log(_this.professionlist)
                    layui.laypage({
                         cont: 'page1', //容器。值支持id名、原生dom对象，jquery对象。【如该容器为】：<div id="page1"></div>
-                        pages: this.totalnum, //通过后台拿到的总页数
-                        curr: this.currpage , //当前页
+                        pages: _this.totalnum, //通过后台拿到的总页数
+                        curr: _this.currpage , //当前页
                         skin: 'yahei', //加载内置皮肤，也可以直接赋值16进制颜色值，如:#c00
                         jump: function (obj, first) { //触发分页后的回调
                             if (!first) { //点击跳页触发函数自身，并传递当前页：obj.curr
-                                this.currpage = obj.curr;
-                                console.log(this.currpage);
-                                fetch();
+                                _this.currpage = obj.curr;
+                                console.log(_this.currpage);
+                                _this.fetch();
                             }
                         }
                     });
